eab267163912d5d2e06227e7913d03e7d3fd7d64
ssongki <ssongki@gmail.com>
2015-11-24 00:23:46 +0900

    - nav ui modify - project ui modify - design ui modify - top ui modify

--- contents/js/custom.js
+++ contents/js/custom.js
@@ -11,6 +11,42 @@ $(document).ready(function() {
 		, $overLay = $('.overlay')
 		, $navIcon = $('#menutoggle')
 		, $mobileNav = $('#topnav ul');
+		
+	
+	// mobile nav
+	$('#toggle').click(function() {
+		$(this).toggleClass('active');
+		$('#overlay').toggleClass('open');
+	});
+	
+	// // top nav 
+	// // Create a clone of the menu, right next to original.
+// $('.topnav').addClass('sticky').clone().insertAfter('.topnav').addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('sticky').hide();
+// 
+// scrollIntervalID = setInterval(stickIt, 10);
+// 
+// 
+// function stickIt() {
+// 
+  // var orgElementPos = $('.sticky').offset();
+  // orgElementTop = orgElementPos.top;               
+// 
+  // if ($(window).scrollTop() >= (orgElementTop)) {
+    // // scrolled past the original position; now only show the cloned, sticky element.
+// 
+    // // Cloned element should always have same left position and width as original element.     
+    // orgElement = $('.sticky');
+    // coordsOrgElement = orgElement.offset();
+    // leftOrgElement = coordsOrgElement.left;  
+    // widthOrgElement = orgElement.css('width');
+    // $('.cloned').css('left',leftOrgElement+'px').css('top',0).css('width',widthOrgElement).show();
+    // $('.sticky').css('visibility','hidden');
+  // } else {
+    // // not scrolled past the menu; only show the original menu.
+    // $('.cloned').hide();
+    // $('.sticky').css('visibility','visible');
+  // }
+// }
 	
 	/* mobile toggle */
 	$navIcon.toggle(function() {
