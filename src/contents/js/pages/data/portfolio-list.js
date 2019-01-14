baf0741081c43647fcce0e66dded52e867f00990
dodortus <dodortus@gmail.com>
2019-01-14 21:04:46 +0900

    * Optimize image

--- src/contents/js/pages/data/portfolio-list.js
+++ src/contents/js/pages/data/portfolio-list.js
@@ -7,61 +7,61 @@ function getPortfolioList() {
     {
       name: "EBS Reading Club",
       path: "ebsrc",
-      imageURL: "/contents/img/portfolio/ebsrc@2x.png",
+      imageURL: "/contents/img/portfolio/ebsrc@2x.jpg",
       description: "Product UI/UX Design, Web/App Design"
     },
     {
       name: "iPortfolio",
       path: "iportfolio",
-      imageURL: "/contents/img/portfolio/ipf@2x.png",
+      imageURL: "/contents/img/portfolio/ipf@2x.jpg",
       description: "Responsive Web Design/Development"
     },
     {
       name: "AR",
       path: "ar",
-      imageURL: "/contents/img/portfolio/ar@2x.png",
+      imageURL: "/contents/img/portfolio/ar@2x.jpg",
       description: "Product UI/UX Design, iOS Design"
     },
     {
       name: "ERA",
       path: "era",
-      imageURL: "/contents/img/portfolio/era@2x.png",
+      imageURL: "/contents/img/portfolio/era@2x.jpg",
       description: "Product UI/UX Design, App Design"
-    },
+    }
     // {
     //   name: "POP Reader",
     //   path: "popleader",
-    //   imageURL: "/contents/img/portfolio/popleader@2x.png",
+    //   imageURL: "/contents/img/portfolio/popleader@2x.jpg",
     //   description: "Product UI/UX Design, Web/App Design"
     // },
     // {
     //   name: "Big Reading Club",
     //   path: "brc",
-    //   imageURL: "/contents/img/portfolio/brc@2x.png",
+    //   imageURL: "/contents/img/portfolio/brc@2x.jpg",
     //   description: "Product UI/UX Design, Web/App Design"
     // },
     // {
     //   name: "Mom Talk",
     //   path: "momtalk",
-    //   imageURL: "/contents/img/portfolio/momtalk@2x.png",
+    //   imageURL: "/contents/img/portfolio/momtalk@2x.jpg",
     //   description: "Responsive Web Design/Development"
     // },
     // {
     //   name: "Mobizen",
     //   path: "mobizen",
-    //   imageURL: "/contents/img/portfolio/mobizen@2x.png",
+    //   imageURL: "/contents/img/portfolio/mobizen@2x.jpg",
     //   description: "Frontend Development (Back Up, Setting, Help)"
     // },
     // {
     //   name: "Mobizen Help",
     //   path: "mobizen-help",
-    //   imageURL: "/contents/img/portfolio/mobizen-help@2x.png",
+    //   imageURL: "/contents/img/portfolio/mobizen-help@2x.jpg",
     //   description: "Frontend Development"
     // },
     // {
     //   name: "Knowre",
     //   path: "knowre",
-    //   imageURL: "/contents/img/portfolio/knowre@2x.png",
+    //   imageURL: "/contents/img/portfolio/knowre@2x.jpg",
     //   description: "Responsive Web Design/Development"
     // }
   ]
