0e6a455def818619924d9eb0fa5a62b83634754f
dodortus <dodortus@gmail.com>
2021-03-20 13:30:01 +0900

    chore: 프로젝트 목록 순서 수정

--- src/contents/js/pages/data/projects-list.js
+++ src/contents/js/pages/data/projects-list.js
@@ -5,14 +5,15 @@
 function getProjectList() {
   return [
     {
-      name: 'PICK',
-      path: 'https://www.behance.net/gallery/109909239/Vote-Application-Cheer-up-youngest',
-      imageURL: '/contents/img/projects/pick@2x.jpg',
-      date: '2020.10 ~ 2020.12',
+      name: 'EBS Reading Club',
+      path: 'ebsrc',
+      imageURL: '/contents/img/projects/ebsrc@2x.jpg',
+      date: '2016.03 ~ 2020.04',
       role: 'Product UI/UX Design',
-      type: 'App Design',
+      type: 'Web/App Design',
       description:
-        '‘PICK’ 앱은 투표 형식의 설문 앱입니다.<br/>근무했던 회사에서는 입사한 지 얼마 안 된 막내가 음료 주문을 했습니다. 저 또한 과거에 그랬던 적이 있었고 매번 일일이 물어보고 답변이 올 때까지 기다리는 답답함과 다양한 주문에 힘들었던 적이 있습니다. 그 경험을 바탕으로 불편함을 개선하기 위한 앱 디자인을 진행했습니다.',
+        'iPortfolio에서 가장 기여를 많이한 프로젝트로 자사 대표 서비스의 UX/UI 디자인 업무를 진행했습니다. ' +
+        'EBS 리딩클럽은 iOS, Android, Web을 제공하고 있는 멀티 플랫폼 서비스입니다.',
     },
     {
       name: 'Woney',
@@ -26,15 +27,14 @@ function getProjectList() {
         'Woney App은 눈에 보이는 자산관리로 실현 가능한 목표를 잡아주고 자산 운용에 대한 방향을 제시합니다.',
     },
     {
-      name: 'EBS Reading Club',
-      path: 'ebsrc',
-      imageURL: '/contents/img/projects/ebsrc@2x.jpg',
-      date: '2016.03 ~ 2020.04',
+      name: 'PICK',
+      path: 'https://www.behance.net/gallery/109909239/Vote-Application-Cheer-up-youngest',
+      imageURL: '/contents/img/projects/pick@2x.jpg',
+      date: '2020.10 ~ 2020.12',
       role: 'Product UI/UX Design',
-      type: 'Web/App Design',
+      type: 'App Design',
       description:
-        'iPortfolio에서 가장 기여를 많이한 프로젝트로 자사 대표 서비스의 UX/UI 디자인 업무를 진행했습니다. ' +
-        'EBS 리딩클럽은 iOS, Android, Web을 제공하고 있는 멀티 플랫폼 서비스입니다.',
+        '‘PICK’ 앱은 투표 형식의 설문 앱입니다.<br/>근무했던 회사에서는 입사한 지 얼마 안 된 막내가 음료 주문을 했습니다. 저 또한 과거에 그랬던 적이 있었고 매번 일일이 물어보고 답변이 올 때까지 기다리는 답답함과 다양한 주문에 힘들었던 적이 있습니다. 그 경험을 바탕으로 불편함을 개선하기 위한 앱 디자인을 진행했습니다.',
     },
     {
       name: 'ReBranding',
@@ -48,17 +48,6 @@ function getProjectList() {
         '주목도 있는 브랜드로 도약하기 위한 리브랜딩 작업을 진행했습니다.',
     },
     {
-      name: 'morlymom',
-      path: 'morlymom',
-      imageURL: '/contents/img/projects/morlymom@2x.jpg',
-      date: '2016.12 ~ 2017.02',
-      role: 'Product UI/UX Design',
-      type: 'App Design',
-      description:
-        '멀리맘 앱은 연령별로 선정된 도서들을 분석해 성장기에 맞춘 자녀에게 도서를 추천하는 큐레이션 서비스 입니다. ' +
-        '메인 디자이너로서 안드로이드 화면 설계 및 UI, 로고 디자인, 사용자 인터페이스 디자인 및 프로토타이핑을 진행했습니다.',
-    },
-    {
       name: 'iPortfolio',
       path: 'iportfolio',
       imageURL: '/contents/img/projects/ipf@2x.jpg',
@@ -68,16 +57,6 @@ function getProjectList() {
       description:
         '회사 브랜드 사이트가 오래전에 개발된 상태로 외부에 공개되는 게 줄곧 신경이 쓰여 작업을 시작하게 되었습니다. ' +
         '서비스 아이덴티티와 화면에 대한 디자인 리뉴얼, 개발을 함께 진행했습니다.',
-    },
-    {
-      name: 'AR',
-      path: 'ar',
-      imageURL: '/contents/img/projects/ar@2x.jpg',
-      date: '2018.05 ~ 2018.07',
-      role: 'Product UI/UX Design',
-      type: 'App Design',
-      description:
-        "매년 참가하는 국제 교육 전시에서 (e.g 'Frankfurt Book Fair', 'London Bett') 신기능을 선보이기 위해 iOS에서 제공하는 AR 기능을 활용한 영어 학습 기능의<br />UI/UX 디자인을 진행했습니다.",
     },
     {
       name: 'Learning Management System',
@@ -91,16 +70,6 @@ function getProjectList() {
         '또 학생들의 학습 과정 관리를 도와 관리자가 진행 상황 파악이 쉽게 도와줍니다.',
     },
     {
-      name: 'MomTalk',
-      path: 'momtalk',
-      imageURL: '/contents/img/projects/momtalk@2x.png',
-      date: '2015.04 ~ 2016.02',
-      role: 'Product UI/UX Design',
-      type: 'App Design',
-      description:
-        '“임신한 순간 똑똑하고 경험 많은 비서를 고용했다”란 슬로건으로 엄마를 위한 서비스 Momtalk의 메인 디자이너로 업무를 진행했습니다.',
-    },
-    {
       name: 'Remember',
       path: 'remember',
       imageURL: '/contents/img/projects/remember@2x.jpg',
@@ -109,6 +78,37 @@ function getProjectList() {
       type: 'Web App 개선 (Side Project)',
       description:
         '현재 서비스에서 아쉬운 점은 복잡한 메뉴 구성과 명함 목록의 개수에 따라 컨테이너들의 높이가 불규칙하게 나열되어 레이아웃이 다소 불안정한 느낌을 받았습니다. PC의 넓은 화면을 활용해 안정감을 주고 관리가 용이한 레이아웃을 제공하는 것이 주요 개선 포인트입니다.',
+    },
+    {
+      name: 'morlymom',
+      path: 'morlymom',
+      imageURL: '/contents/img/projects/morlymom@2x.jpg',
+      date: '2016.12 ~ 2017.02',
+      role: 'Product UI/UX Design',
+      type: 'App Design',
+      description:
+        '멀리맘 앱은 연령별로 선정된 도서들을 분석해 성장기에 맞춘 자녀에게 도서를 추천하는 큐레이션 서비스 입니다. ' +
+        '메인 디자이너로서 안드로이드 화면 설계 및 UI, 로고 디자인, 사용자 인터페이스 디자인 및 프로토타이핑을 진행했습니다.',
+    },
+    {
+      name: 'AR',
+      path: 'ar',
+      imageURL: '/contents/img/projects/ar@2x.jpg',
+      date: '2018.05 ~ 2018.07',
+      role: 'Product UI/UX Design',
+      type: 'App Design',
+      description:
+        "매년 참가하는 국제 교육 전시에서 (e.g 'Frankfurt Book Fair', 'London Bett') 신기능을 선보이기 위해 iOS에서 제공하는 AR 기능을 활용한 영어 학습 기능의<br />UI/UX 디자인을 진행했습니다.",
+    },
+    {
+      name: 'MomTalk',
+      path: 'momtalk',
+      imageURL: '/contents/img/projects/momtalk@2x.png',
+      date: '2015.04 ~ 2016.02',
+      role: 'Product UI/UX Design',
+      type: 'App Design',
+      description:
+        '“임신한 순간 똑똑하고 경험 많은 비서를 고용했다”란 슬로건으로 엄마를 위한 서비스 Momtalk의 메인 디자이너로 업무를 진행했습니다.',
     },
   ];
 }
